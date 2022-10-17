f79b32926e58700342942cbb8ca698fac66e0710
Alperen Atar <71658129+EmparrioR@users.noreply.github.com>
2022-10-17 22:57:05 +0300

    Add files via upload

---
+++ En_Buyuk_Kucuk_Bulma.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class En_Buyuk_Kucuk_Bulma {
+	public static void main(String [] args){
+		
+		
+		
+		Scanner inp = new Scanner(System.in);
+		
+		System.out.print("Kaç tane sayı gireceksiniz ?: ");
+		int no = inp.nextInt();
+		
+		int maks = 0,min = 0;
+		
+		for (int i=1; i<=no; i++)
+        {
+            System.out.print(i+". sayiyi giriniz: ");
+            int sayi = inp.nextInt();
+            
+            if (i==1)
+            {
+                maks=sayi;
+                min=sayi;
+            }
+            
+            if (sayi>maks)
+                maks=sayi;
+            
+            if (sayi<min)
+                min=sayi;
+        }
+		
+			System.out.println("En Büyük Sayı: " + maks);
+			System.out.println("En Küçük Sayı: " + min);
+	}
+}
