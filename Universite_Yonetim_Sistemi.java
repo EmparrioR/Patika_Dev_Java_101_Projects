5db1a8a579c98874c721c0e91963361335e68825
Alperen Atar <71658129+EmparrioR@users.noreply.github.com>
2022-11-29 22:12:37 +0300

    Add files via upload

---
+++ Universite_Yonetim_Sistemi.java
@@ -0,0 +1,35 @@
+class Universite{
+
+}
+
+class Classes extends Universite{
+
+}
+
+class WorkOffice extends Universite{
+
+}
+
+class Departments extends Universite{
+
+}
+
+class Offices extends Departments {
+
+}
+
+
+class Professors extends Offices{
+
+}
+
+class Memur extends Offices{
+
+}
+
+
+public class Universite_Yonetim_Sistemi{
+    public static void main(String [] args) {
+        
+    }
+}
